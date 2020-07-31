00e112f84e0a3c5d736316ed12e83cb470c0de32
DTR <t.terry.rice@gmail.com>
2020-07-31 18:00:41 +0300

    header roof

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//about
+
+const about = document.querySelector('#about');
+
+about.addEventListener('click', (event) => {
+  const dropDown = document.querySelector('.roof__navigation-drop');
+
+  event.preventDefault();
+
+  dropDown.style = `
+    visibility: visible;
+    opacity: 1;
+  `;
+});
+
+document.addEventListener('click', (event) => { 
+  if (!about.contains(event.target)) {
+    const dropDown = document.querySelector('.roof__navigation-drop');
+
+    dropDown.style = `
+      visibility: hidden;
+      opacity: 0;
+    `;
+  }
+});
