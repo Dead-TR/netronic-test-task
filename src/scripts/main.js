168d68028ecb4c91721d316a57826324f145d1e3
DTR <t.terry.rice@gmail.com>
2020-08-04 15:14:26 +0300

    final desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
-//about
-
+// about
 const about = document.querySelector('#about');
 
 about.addEventListener('click', (event) => {
@@ -23,5 +22,98 @@ document.addEventListener('click', (event) => {
       visibility: hidden;
       opacity: 0;
     `;
+  }
+});
+
+// full video
+const videoLink = document.querySelector('.header__URL-video');
+const crossVideo = document.querySelector('.full-video__cross');
+
+const videoDisplay = (videoAction, overlapAction) => {
+  const videoObj = document.querySelector('.full-video');
+  const overlap = document.querySelector('.overlap');
+
+  overlap.style = `display: ${overlapAction}`;
+  videoObj.style = `display: ${videoAction}`;
+};
+
+videoLink.addEventListener('click', () => {
+  videoDisplay('flex', 'block');
+});
+
+crossVideo.addEventListener('click', () => {
+  videoDisplay('none', 'none');
+});
+
+// tour card
+const tour = [...document.querySelector('.tour').children];
+
+for (const tourChild of tour) {
+  tourChild.style = `bottom: -${tourChild.offsetHeight / 2}px;`;
+}
+
+// toggle
+const toggleButton = document.querySelector('.price-card__toggle');
+const price = {
+  '60min': {
+    0: '3 000 ₽',
+    1: '3 600 ₽',
+  },
+  '30min': {
+    0: '1 500 ₽',
+    1: '1 800 ₽',
+  },
+};
+const add = {
+  '60min': {
+    0: '+500 ₽',
+    1: '+500 ₽',
+  },
+  '30min': {
+    0: '+250 ₽',
+    1: '+300 ₽',
+  },
+};
+
+toggleButton.addEventListener('click', (event) => {
+  const timesText = [
+    ...document.querySelectorAll('.price__command-game .price-card__time'),
+  ];
+  const prices = [
+    ...document.querySelectorAll('.price__command-game .price-card__price'),
+  ];
+  const addPrice = [
+    ...document.querySelectorAll('.price__command-game .price-card__add'),
+  ];
+
+  if (event.target.dataset.active === '60min') {
+    toggleButton.dataset.active = '30min';
+    toggleButton.src = './images/main/price/price-card/toggle_off.svg';
+  } else {
+    toggleButton.dataset.active = '60min';
+    toggleButton.src = './images/main/price/price-card/toggle_on.svg';
+  }
+
+  for (const i of timesText) {
+    i.classList.toggle('price-card_gray');
+  }
+
+  for (let i = 0; i < prices.length; i++) {
+    prices[i]
+      .lastElementChild.textContent = price[event.target.dataset.active][i];
+
+    addPrice[i]
+      .firstElementChild.textContent = add[event.target.dataset.active][i];
+  }
+});
+
+// form 
+const oForm = document.querySelector('.o-form');
+
+oForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const i of event.target.children) {
+    i.value = ''; // Sand To Server Script
   }
 });
